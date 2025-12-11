3a4054217f19ae53c6803f9009f564acb404aa20
UNEXIS <106434295+UNEXISDevelopment@users.noreply.github.com>
2025-12-11 15:38:35 +0200

    Add files via upload

---
+++ unexis_saflags/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'unexis'
+description 'SAFLAGS'
+version '1.0.0'
+lua54 'yes'
+this_is_a_map 'yes'
+
+
+server_script 'server.lua' -- Version and Resource Name Check
+
+
+
+dependency '/assetpacks'
+
+
+
